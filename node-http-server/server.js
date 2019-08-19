ccdae69edd7f02f6c5765fbaede5e0b2129a6774
Alejandro Salas <asalas.wd@outlook.com>
2019-08-18 23:33:43 -0400

    start express example

--- node-http-server/server.js
+++ node-http-server/server.js
@@ -1,4 +1,14 @@
 const http = require('http');
-const server = http.createServer();
+const server = http.createServer( (req, res) => {
+  console.log(`Someone has requested ${req.url}`);
 
-server.lis
+  if (req.url === '/'){
+    res.write('This is my page!');
+    res.end();
+  } else {
+    res.write('Sorry, dude. Totally a 404');
+    res.end();
+  }
+});
+
+server.listen(3000);
